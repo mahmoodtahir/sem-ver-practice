f4779270d6b0b6b0309893efe08e1720761a9660
RTI\mtahir <mahmood.t@ri-team.com>
2020-09-15 11:44:42 +0100

    test commit

--- SemVerPractice/Controllers/HomeController.cs
+++ SemVerPractice/Controllers/HomeController.cs
@@ -10,6 +10,18 @@ namespace SemVerPractice.Controllers
     {
         public ActionResult Index()
         {
+            System.Reflection.Assembly assembly = typeof(HomeController).Assembly;
+
+            var assemblyName = assembly.GetName().Name;
+            var gitVersionInformationType = assembly.GetType("GitVersionInformation");
+            var fields = gitVersionInformationType.GetFields();
+
+            foreach (var field in fields)
+            {
+                Console.WriteLine(string.Format("{0}: {1}", field.Name, field.GetValue(null)));
+            }
+
+
             ViewBag.Title = "Home Page";
 
             return View();
